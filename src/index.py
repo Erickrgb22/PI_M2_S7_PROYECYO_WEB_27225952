9332127bfc9f72555fb2d64e19c42bddf058b85e
Erick Gilmore <erick.rgb.00@gmail.com>
2024-04-27 17:51:37 -0400

    Añadida Navbar

--- src/index.py
+++ src/index.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__ , static_url_path='/static')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
    return render_template('home.html')
 
 @app.route('/about')
 def about():
    return render_template('about.html')
+
+@app.route('/merchant')
+def merchant():
+   return render_template('merchant.html')
+
+@app.route('/register')
+def register():
+   return render_template('register.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
