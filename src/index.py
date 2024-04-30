9ceee1fa0cb7264a6b4f50496ba1bd53ec9ac288
Erick Gilmore <erick.rgb.00@gmail.com>
2024-04-30 02:08:46 -0400

    OTRO COMMIT A LAS 2 AM

--- src/index.py
+++ src/index.py
@@ -6,26 +6,13 @@ app = Flask(__name__ , static_url_path='/static')
 def home():
    return render_template('home.html')
 
-@app.route('/about')
-def about():
-   return render_template('about.html')
-
-@app.route('/cars')
-def merchant():
-   return render_template('cars.html')
-
-@app.route('/merchandising')
-def merchandising():
-   return render_template('merchandising.html')
-
 @app.route('/motorsport')
-def register():
+def motorsport():
    return render_template('motorsport.html')
 
-@app.route('/team')
-def team():
-   return render_template('team.html')
-
+@app.route('/motorrad')
+def   motorrad():
+      return render_template('motorrad.html')
 @app.route('/academy')
 def academy():
    return render_template('academy.html')
